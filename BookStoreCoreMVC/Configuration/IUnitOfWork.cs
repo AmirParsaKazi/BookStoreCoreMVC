e7ddf7b214d22fb6c4438703c8b6e39665bcf319
Amirparsa <parsakziiii@gmail.com>
2023-06-07 15:20:22 +0330

    Add Unit Of Works

---
+++ BookStoreCoreMVC/Configuration/IUnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using BookStoreCoreMVC.Models.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreCoreMVC.Configuration;
+
+public interface IUnitOfWork
+{
+    IAuthorRepository Author { get; }
+    IBookRepository Book { get; }
+    ICategoryRepository Category { get; }
+    ICityRepository City { get; }
+    ICustomerRepository Customer { get; }
+    IDiscountRepository Discount { get; }
+    ILanguageRepository Language { get; }
+    IOrderRepository Order { get; }
+    IOrderStatusRepository OrderStatus { get; }
+    IProvinceRepository Province { get; }
+
+    Task CompleteAsync();
+
+    Task Dispose();
+}
---
+++ BookStoreCoreMVC/Configuration/UnitOfWork.cs
@@ -0,0 +1,68 @@
+﻿using BookStoreCoreMVC.Models.Interfaces;
+using BookStoreCoreMVC.Data;
+using BookStoreCoreMVC.Repositories;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStoreCoreMVC.Configuration;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDBContext _context;
+    private readonly ILogger _logger;
+   
+    public IAuthorRepository Author { get; private set; }
+    public IBookRepository Book { get; private set; }
+    public ICategoryRepository Category { get; private set; }
+    public ICityRepository City { get; private set; }
+    public ICustomerRepository Customer { get; private set; }
+    public IDiscountRepository Discount { get; private set; }
+    public ILanguageRepository Language { get; private set; }
+    public IOrderRepository Order { get; private set; }
+    public IOrderStatusRepository OrderStatus { get; private set; }
+    public IProvinceRepository Province { get; private set; }
+
+    public UnitOfWork(ApplicationDBContext context, ILoggerFactory loggerFactory)
+    {
+        _context = context;
+        _logger = loggerFactory.CreateLogger("logs");
+
+        Author = new AuthorRepository(context, _logger);
+        Book = new BookRepository(context, _logger);
+        Category = new CategoryRepository(context, _logger);
+        City = new CityRepository(context, _logger);
+        Customer = new CustomerRepository(context, _logger);
+        Discount = new DiscountRepository(context, _logger);
+        Language = new LanguageRepository(context, _logger);
+        Order = new OrderRepository(context, _logger);
+        OrderStatus = new OrderStatusRepository(context, _logger);
+        Province = new ProvinceRepository(context, _logger);
+    }
+
+    public async Task CompleteAsync()
+    {
+        try{
+            await _context.SaveChangesAsync();
+        }
+        catch (Exception er)
+        {
+            _logger.LogError(er, er.Message, typeof(UnitOfWork));
+        }
+    }
+
+    public async Task Dispose()
+    {
+        try
+        {
+            await _context.DisposeAsync();
+        }
+        catch (Exception er)
+        {
+            _logger.LogError(er, er.Message, typeof(UnitOfWork));
+        }
+    }
+}
