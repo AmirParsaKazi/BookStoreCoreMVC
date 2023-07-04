9a9e4f1c6e2598fe6b90c88ae9007e40df52804b
Amirparsa <parsakziiii@gmail.com>
2023-07-04 21:11:12 +0330

    create book controller

---
+++ BookStoreCoreMVC/Areas/Admin/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using BookStoreCoreMVC.Models.ViewModels.Category;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreCoreMVC.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class BookController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
+      
+        return View();
+    }
+}
+
